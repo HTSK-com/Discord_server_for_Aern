8175059abfe2b8a6e9e83ac40bff28a5c2759988
KopytovMoscow <57916666+KopytovMoscow@users.noreply.github.com>
2020-10-22 12:58:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,22 @@ async def poll(ctx, *, text):
     message = await ctx.send(text)
     for emoji in ('🤡', '💩'):
         await message.add_reaction(emoji)
+        
+@bot.event
+async def on_message(message):
+    if message.content.startswith('$thumb'):
+        channel = message.channel
+        await channel.send('Send me that 👍 reaction, mate')
+
+        def check(reaction, user):
+            return user == message.author and str(reaction.emoji) == '👍'
+
+        try:
+            reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await channel.send('👎')
+        else:
+            await channel.send('👍')        
 
 
 
